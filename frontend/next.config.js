8b2a8db98d6b1c37983e7f08874b9fedcb7e0a3f
Vasantha Kumara <vasantha.cd22@bmsce.ac.in>
2026-06-03 22:49:43 +0530

    Add Next.js configuration

---
+++ frontend/next.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
